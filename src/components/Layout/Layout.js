d4c99691b67429ae39f6c69ad960f06414d2ec95
Akash <akashleo2009@gmail.com>
2021-04-28 00:38:23 +0530

    Logo added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Aux from "../../hoc/Auxilliary";
 import "./Layout.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 const layout = (props) => (
   <Aux>
-    <div>ToolBar, SideDrawer, Backdrop</div>
+    <Toolbar />
     <main className="Content">{props.children}</main>
   </Aux>
 );
